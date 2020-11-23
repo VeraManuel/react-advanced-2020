69999d27b2e0bf492ae28e5f6a2758b047b95c90
Manuel Vera <manuelvera202@gmail.com>
2020-11-23 12:34:40 -0300

    finish tutorial

--- src/tutorial/11-react-router/setup/Home.js
+++ src/tutorial/11-react-router/setup/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>This is my home page in router</p>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
